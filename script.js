29490353ac0458a0256e36b03e1cc341c7eea7bb
Susy2801 <154439974+Susy2801@users.noreply.github.com>
2024-01-31 05:23:27 +0700

    Update script.js
    add tele_bot

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 getData1 = async (page) => {
-  let jsonData = await fetch(
-    `https://ophim1.com/danh-sach/phim-moi-cap-nhat?page=${page}`
-  );
-  let data = await jsonData.json();
-  return data;
+  try {
+    let jsonData = await fetch(
+      `https://ophim1.com/danh-sach/phim-moi-cap-nhat?page=${page}`
+    );
+    let data = await jsonData.json();
+    return data;
+  } catch (error) {
+    console.log("Get Api1 lỗi");
+  }
 };
 
 async function fetchApi2(slug) {
-  let dataJson = await fetch(`https://ophim1.com/phim/${slug}`);
-  let data = await dataJson.json();
-  return data;
+  try {
+    let dataJson = await fetch(`https://ophim1.com/phim/${slug}`);
+    let data = await dataJson.json();
+    return data;
+  } catch (error) {
+    console.log("Get Api2 lỗi");
+  }
 }
 
 var page = 1;
@@ -18,6 +26,7 @@ function dataApi2() {
     getData1(page)
       .then((result) => {
         let items = result.items;
+
         let allData = [];
         items.forEach((items) => {
           slug = items.slug;
@@ -48,6 +57,7 @@ function Render() {
     // In ra tập phim
     if (phimHan !== undefined) {
       var movieLink = phimHan.episodes[0].server_data;
+      console.log(movieLink);
       var pageBox = document.querySelector(".episodes");
 
       movieLink.forEach((link, index) => {
@@ -61,6 +71,16 @@ function Render() {
           var screen = document.querySelector(".screen");
           screen.src = link.link_embed;
         };
+
+        if (index + 1 > 10) {
+          const chatId = "-4144196603";
+          const messageData = {
+            chat_id: chatId,
+            text: `Có tập ${index + 1} rồi nè bé ơi!!!`,
+          };
+
+          sendMessage(messageData);
+        }
       });
     } else {
       var pageBox = document.querySelector(".episodes");
@@ -75,4 +95,22 @@ Render();
 function findFilm() {
   page++;
   Render();
+}
+
+// TELEGRAM BOT
+
+// Gửi yêu cầu POST đến Telegram API
+function sendMessage(messageData) {
+  var botToken = "6707949403:AAEIYlrBGNVeFO7ugkemna3svdabpuHwIbM";
+  const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(messageData),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log("Phản hồi từ Telegram API:", data))
+    .catch((error) => console.error("Lỗi khi gửi yêu cầu:", error));
 }
